c2137933a4c36e29275774f5ef51378c8670cdf0
CaptainZiboo <96137708+CaptainZiboo@users.noreply.github.com>
2025-08-01 09:18:09 +0200

    fix add missing lambda file

---
+++ terraform/lambda.tf
@@ -0,0 +1,24 @@
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/TodosLambdaFunction"
+  retention_in_days = 7
+  tags              = var.tags
+}
+
+resource "aws_lambda_function" "lambda_function_over_https" {
+  filename         = "${path.module}/lambda/lambda_function_payload.zip"
+  function_name    = "TodosLambdaFunction"
+  role             = aws_iam_role.lambda_apigateway_role.arn
+  handler          = "index.handler"
+  source_code_hash = filebase64sha256("${path.module}/lambda/lambda_function_payload.zip")
+  runtime          = "nodejs20.x"
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.todos_table.name
+    }
+  }
+
+  tags = var.tags
+
+  depends_on = [aws_cloudwatch_log_group.lambda_logs]
+}
